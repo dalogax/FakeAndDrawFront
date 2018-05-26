c926dc407d53cd4987ae7c7cd033c89649d427c1
Edgaras Makarovas <e.k.makarovas@gmail.com>
2018-05-26 20:08:42 +0200

    Added basic server communication functions.

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,28 @@
+import SockJS from 'sockjs-client';
+import Stomp from "@stomp/stompjs";
 
-export function connectToServer(serverEndpoint, onMessageReceived) {
-    setTimeout(() => {
-        onMessageReceived({
-            "type": "game-created",
-            "body": {
-                "codeGame": "HFKDC",
-                "lifespanTimestamp": Date.now() + 10000
-            }
+const serverUrl = "http://localhost:8080/fakeanddraw";
+const serverRequestUrl = "/app/request";
+var stompClient = null;
+
+export function connectToServer(onMessageReceived) {
+    var socket = new SockJS(serverUrl);
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        var sessionId = /\/([^\/]+)\/websocket/.exec(socket._transport.url)[1];
+        const responseUrl = generateServerResponseUrl(sessionId);
+        stompClient.subscribe(responseUrl, function (greeting) {
+            onMessageReceived(greeting.body);
         });
-    }, 5000);
+    });
 }
+
+export function sendToServer(obj) {
+    stompClient.send(serverRequestUrl, {}, JSON.stringify(obj));
+}
+
+function generateServerResponseUrl(sessionId) {
+    return '/user/' + sessionId + '/topic/response';
+}
+
+connectToServer(new function() {});
