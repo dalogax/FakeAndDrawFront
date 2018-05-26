38d4db167225763d087f2a14b19d6859b4d14e8a
Paquitosoft <paquitosoftware@gmail.com>
2018-05-26 12:06:40 +0200

    Created skeletons for each channel home pages

---
+++ src/pages/manager/home/home-view.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <section className="manager-home-page">
+                <h1>Fake & Draw!!!</h1>
+            </section>
+        );
+    }
+
+}
+
+export default Home;
